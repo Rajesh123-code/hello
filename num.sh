51444ef93321d3351234d6b24242bdf0adeae97d
Rajesh <rajesh23@gmail>
2024-07-24 12:39:24 +0530

    Trigger

--- num.sh
+++ num.sh
@@ -1,7 +1,6 @@
-for i in {1..100};
-do  
-    if ([ 'expr $i % 3' == 0 ] || [ 'expr $i % 5' == 0 ]) && [ 'expr $i % 15' != 0 ]
-    then 
-        echo $i 
-    fi;
+for i in {1..100}; do 
+if ([ 'expr $i % 3' == 0 ] || [ 'expr $i % 5' == 0 ]) && [ 'expr $i % 15' != 0 ];
+then 
+    echo $i 
+fi;
 done 
