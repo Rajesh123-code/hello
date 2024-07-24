1a9e1b3931645efcafa3aa454e6d23bd66be61a3
Rajesh <rajesh23@gmail>
2024-07-24 11:29:05 +0530

    Trigger

--- ui.sh
+++ ui.sh
@@ -3,8 +3,11 @@
 echo -n "Enter a line of text: "
 read string
 
-numCount=$(echo $string | grep -o "[0-9]" | wc --lines)
+#numCount=$(echo $string | grep -o "[0-9]" | wc --lines)
 vowCount=$(echo $string | grep -o -i "[aeiou]" | wc --lines)
-consCount=$(echo $string | grep -o -i "[bcdfghjklmnpqrstvwxyz]" | wc --lines)
+#consCount=$(echo $string | grep -o -i "[bcdfghjklmnpqrstvwxyz]" | wc --lines)
 
-echo "The given string has $vowCount vowels, $consCount consonants and $numCount numbers in it."
+#echo "The given string has $vowCount vowels, $consCount consonants and $numCount numbers in it."
+
+echo "This given string has $vowCount vowels"
+
